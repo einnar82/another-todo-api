a161408d6515816e1edc182590eba6e7af04a395
Rannie Ollit <rannieollit@Rannies-MacBook-Pro.local>
2024-07-08 05:21:53 +0800

    added allowed origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://another-todo-app-five.vercel.app'
+    ],
 
     'allowed_origins_patterns' => [],
 
